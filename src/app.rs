8a4d02c4afd605ef8b8774f6ea9da3f23691e1a0
Clicia Scarlet <yuyuvn@mac.com>
2020-04-01 15:20:58 +0900

    Fetch and render data

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,42 @@
+mod helpers;
+
 use log::*;
 use serde_derive::{Deserialize, Serialize};
 // use strum::IntoEnumIterator;
 use strum_macros::{EnumIter, ToString};
 // use yew::format::Json;
 use yew::services::storage::{Area, StorageService};
+use yew::services::fetch::{FetchService, FetchTask, Request, Response};
+use yew::format::{Json, Nothing};
 use yew::prelude::*;
+use anyhow::Error;
+use std::collections::HashMap;
+
+const API_ENDPOINT: &str = "https://pomber.github.io/covid19/timeseries.json";
+
+type AsBinary = bool;
 
 pub struct App {
     link: ComponentLink<Self>,
     storage: StorageService,
     state: State,
+    fetch_service: FetchService,
+    ft: Option<FetchTask>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct State {
     countries: Vec<Country>,
     sort: Sort,
-    page: usize
+    page: usize,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Country {
-    rank: usize,
-    last_rank: usize,
+    name: String,
+    rank: isize,
+    last_rank: isize,
+    rank_changed: isize,
     confirmed: usize,
     deaths: usize,
     recovered: usize
@@ -30,10 +44,23 @@ pub struct Country {
 
 pub enum Msg {
     SetSort(Sort),
-    SetPage(usize)
+    SetPage(usize),
+    FetchData,
+    FetchReady(Result<RawCountryData, Error>),
+    Ignore,
 }
 
-#[derive(EnumIter, ToString, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+pub struct RawCountry {
+    // date: String, skip to derive Copy
+    confirmed: usize,
+    deaths: usize,
+    recovered: usize,
+}
+
+type RawCountryData = HashMap<String, Vec<RawCountry>>;
+
+#[derive(EnumIter, ToString, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Sort {
     Rank,
     RankRevered,
@@ -53,15 +80,22 @@ impl Component for App {
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
         let storage = StorageService::new(Area::Local).unwrap();
+
         let countries = Vec::new();
         let state = State {
             countries,
             sort: Sort::Rank,
-            page: 1,
+            page: 1
         };
-        App { link, storage, state }
+        let fetch_service = FetchService::new();
+        App { fetch_service, link, storage, state, ft: None, }
     }
 
+    fn mounted(&mut self) -> ShouldRender {
+        self.fetch_json();
+
+        true
+    }
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::SetPage(page) => {
@@ -69,6 +103,42 @@ impl Component for App {
             }
             Msg::SetSort(sort) => {
                 self.state.sort = sort;
+                self.state.sort();
+            }
+            Msg::FetchData => {
+                self.fetch_json();
+            }
+            Msg::FetchReady(response) => {
+                let data = response.ok();
+                match data {
+                    Some(d) => {
+                        info!("{:?}", d);
+                        let mut countries = Vec::new();
+                        for (k, v) in &d {
+                            let n = v.len();
+                            let (rank, last_rank) = self.rank_country(k.as_str(), &d);
+                            let rank_changed = last_rank - rank;
+                            let country = Country {
+                                name: k.to_string(),
+                                rank,
+                                last_rank,
+                                rank_changed,
+                                confirmed: v[n - 1].confirmed,
+                                deaths: v[n - 1].deaths,
+                                recovered: v[n - 1].recovered,
+                            };
+                            countries.insert(0, country);
+                        }
+                        self.state.countries = countries;
+                        self.state.sort();
+                    }
+                    None => {
+                        // do nothing
+                    }
+                }
+            }
+            Msg::Ignore => {
+                return false;
             }
         }
         true
@@ -90,39 +160,21 @@ impl Component for App {
                     <div class="rank-table-header">{ "+/-" } <span class="arrows"></span></div>
                     <div class="rank-table-header">{ "Positions" }</div>
 
-                    <div class="rank-table-data">{ "1" }</div>
-                    <div class="rank-table-data rank-table-data-country"><img alt="USA" src="https://cdn.staticaly.com/gh/hjnilsson/country-flags/master/svg/us.svg" /> {"United States"}</div>
-                    <div class="rank-table-data">{"142,537"}</div>
-                    <div class="rank-table-data">{"2,510"}</div>
-                    <div class="rank-table-data">{"4,767"}</div>
-                    <div class="rank-table-data">{"0 "}</div>
-                    <div class="rank-table-data"><span class="arrows"></span></div>
-
-                    <div class="rank-table-data">{"2"}</div>
-                    <div class="rank-table-data rank-table-data-country"><img alt="Italia" src="https://cdn.staticaly.com/gh/hjnilsson/country-flags/master/svg/it.svg" /> {"Italy"}</div>
-                    <div class="rank-table-data">{"97,689"}</div>
-                    <div class="rank-table-data">{"10,779"}</div>
-                    <div class="rank-table-data">{"13,030"}</div>
-                    <div class="rank-table-data">{"1 "}</div>
-                    <div class="rank-table-data"><span class="arrows up"></span></div>
-
-                    <div class="rank-table-data">{"3"}</div>
-                    <div class="rank-table-data rank-table-data-country"><img alt="China" src="https://cdn.staticaly.com/gh/hjnilsson/country-flags/master/svg/cn.svg" /> {"China"}</div>
-                    <div class="rank-table-data">{"81,470"}</div>
-                    <div class="rank-table-data">{"3,304"}</div>
-                    <div class="rank-table-data">{"75,770"}</div>
-                    <div class="rank-table-data">{"-1 "}</div>
-                    <div class="rank-table-data"><span class="arrows down"></span></div>
+                    {
+                        for self.state.countries.iter()
+                            .enumerate()
+                            .map(|val| self.view_country(val))
+                    }
                 </section>
-                <section class="rank-table-paginate">
-                    <ul class="pagination">
-                        <li class="paginate-button active"><a href="#">{"1"}</a></li>
-                        <li class="paginate-button"><a href="#">{"2"}</a></li>
-                        <li class="paginate-button"><a href="#">{"3"}</a></li>
-                        <li class="paginate-button"><a href="#">{"4"}</a></li>
-                        <li class="paginate-button"><a href="#">{"5"}</a></li>
-                    </ul>
-                </section>
+                // <section class="rank-table-paginate">
+                //     <ul class="pagination">
+                //         <li class="paginate-button active"><a href="#">{"1"}</a></li>
+                //         <li class="paginate-button"><a href="#">{"2"}</a></li>
+                //         <li class="paginate-button"><a href="#">{"3"}</a></li>
+                //         <li class="paginate-button"><a href="#">{"4"}</a></li>
+                //         <li class="paginate-button"><a href="#">{"5"}</a></li>
+                //     </ul>
+                // </section>
                 <footer class="info">
                     <p>{ "Written by " }<a href="https://github.com/yuyuvn/" target="_blank">{ "Clicia Scarlet" }</a></p>
                 </footer>
@@ -133,7 +185,71 @@ impl Component for App {
 
 
 impl App {
+    fn fetch_json(&mut self) {
+        let callback = self.link.callback(
+            move |response: Response<Json<Result<RawCountryData, Error>>>| {
+                let (meta, Json(data)) = response.into_parts();
+                println!("META: {:?}, {:?}", meta, data);
+                if meta.status.is_success() {
+                    Msg::FetchReady(data)
+                } else {
+                    error!("Error: {:?}", data);
+                    Msg::Ignore
+                }
+            },
+        );
+        let request = Request::get(API_ENDPOINT).body(Nothing).unwrap();
+        let task = self.fetch_service.fetch(request, callback).unwrap();
+        self.ft = Some(task);
+    }
 
+    fn rank_country(&mut self, country: &str, data: &RawCountryData) -> (isize, isize) {
+        let mut rank_today = 1;
+        let mut rank_yesterday = 1;
+        let country_len = data[country].len();
+        let today_data = data[country][country_len - 1];
+        let yesterday_data = data[country][country_len - 2];
+        for (_, v) in data {
+            let len = v.len();
+            if v[len-1].confirmed > today_data.confirmed {
+                rank_today += 1;
+            }
+            if v[len-2].confirmed > yesterday_data.confirmed {
+                rank_yesterday += 1;
+            }
+        }
+        return (rank_today, rank_yesterday)
+    }
+
+    fn view_country(&self, (_, entry): (usize, &Country)) -> Html {
+        html! {
+            <>
+                <div class="rank-table-data">{ entry.rank }</div>
+                <div class="rank-table-data rank-table-data-country"><img alt={ entry.name.as_str() } src={ helpers::get_country_code(entry.name.as_str()) } /> { entry.name.as_str() }</div>
+                <div class="rank-table-data">{ entry.confirmed }</div>
+                <div class="rank-table-data">{ entry.deaths }</div>
+                <div class="rank-table-data">{ entry.recovered }</div>
+                <div class="rank-table-data">{ entry.rank_changed } </div>
+                <div class="rank-table-data">{ self.view_diff(entry.rank_changed) }</div>
+            </>
+        }
+    }
+
+    fn view_diff(&self, diff: isize) -> Html {
+        if diff > 0 {
+            html! {
+                <span class="arrows up"></span>
+            }
+        } else if diff == 0 {
+            html! {
+                <span class="arrows"></span>
+            }
+        } else {
+            html! {
+                <span class="arrows down"></span>
+            }
+        }
+    }
 }
 
 impl<'a> Into<Href> for &'a Sort {
@@ -158,5 +274,18 @@ impl Sort {
 }
 
 impl State {
-
+    fn sort(&mut self) {
+        match self.sort {
+            Sort::Rank => self.countries.sort_by(|a, b| a.rank.partial_cmp(&b.rank).unwrap()),
+            Sort::RankRevered => self.countries.sort_by(|a, b| b.rank.partial_cmp(&a.rank).unwrap()),
+            Sort::Confirmed => self.countries.sort_by(|a, b| a.confirmed.partial_cmp(&b.confirmed).unwrap()),
+            Sort::ConfirmedRevered => self.countries.sort_by(|a, b| b.confirmed.partial_cmp(&a.confirmed).unwrap()),
+            Sort::Deaths => self.countries.sort_by(|a, b| a.deaths.partial_cmp(&b.deaths).unwrap()),
+            Sort::DeathsRevered => self.countries.sort_by(|a, b| b.deaths.partial_cmp(&a.deaths).unwrap()),
+            Sort::Recovered => self.countries.sort_by(|a, b| a.recovered.partial_cmp(&b.recovered).unwrap()),
+            Sort::RecoveredRevered => self.countries.sort_by(|a, b| b.recovered.partial_cmp(&a.recovered).unwrap()),
+            Sort::Changed => self.countries.sort_by(|a, b| a.rank_changed.partial_cmp(&b.rank_changed).unwrap()),
+            Sort::ChangedRevered => self.countries.sort_by(|a, b| b.rank_changed.partial_cmp(&a.rank_changed).unwrap()),
+        }
+    }
 }
