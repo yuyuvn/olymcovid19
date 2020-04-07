5e00a5f463aa6501270956507a448cc1d93d8f70
Clicia "CC" Scarlet <yuyuvn@users.noreply.github.com>
2020-04-07 14:40:15 +0900

    Implement sort

--- src/app.rs
+++ src/app.rs
@@ -14,8 +14,6 @@ use std::collections::HashMap;
 
 const API_ENDPOINT: &str = "https://pomber.github.io/covid19/timeseries.json";
 
-type AsBinary = bool;
-
 pub struct App {
     link: ComponentLink<Self>,
     storage: StorageService,
@@ -45,7 +43,6 @@ pub struct Country {
 pub enum Msg {
     SetSort(Sort),
     SetPage(usize),
-    FetchData,
     FetchReady(Result<RawCountryData, Error>),
     Ignore,
 }
@@ -63,15 +60,17 @@ type RawCountryData = HashMap<String, Vec<RawCountry>>;
 #[derive(EnumIter, ToString, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Sort {
     Rank,
-    RankRevered,
+    RankReversed,
+    Country,
+    CountryReversed,
     Confirmed,
-    ConfirmedRevered,
+    ConfirmedReversed,
     Deaths,
-    DeathsRevered,
+    DeathsReversed,
     Recovered,
-    RecoveredRevered,
+    RecoveredReversed,
     Changed,
-    ChangedRevered
+    ChangedReversed
 }
 
 impl Component for App {
@@ -104,9 +103,6 @@ impl Component for App {
             Msg::SetSort(sort) => {
                 self.state.sort = sort;
                 self.state.sort();
-            }
-            Msg::FetchData => {
-                self.fetch_json();
             }
             Msg::FetchReady(response) => {
                 let data = response.ok();
@@ -152,12 +148,12 @@ impl Component for App {
                     <h1>{ "Olymcovid19 ranking" }</h1>
                 </header>
                 <section class="rank-table">
-                    <div class="rank-table-header">{ "Rank" } <span class="arrows down"></span></div>
-                    <div class="rank-table-header">{ "Country" } <span class="arrows"></span></div>
-                    <div class="rank-table-header autohide">{ "Confirmed" } <span class="arrows"></span></div>
-                    <div class="rank-table-header autohide">{ "Deaths" } <span class="arrows"></span></div>
-                    <div class="rank-table-header autohide">{ "Recovered" } <span class="arrows"></span></div>
-                    <div class="rank-table-header">{ "+/-" } <span class="arrows"></span></div>
+                    <div class="rank-table-header">{ self.view_header("Rank", Sort::Rank) }</div>
+                    <div class="rank-table-header">{ self.view_header("Country", Sort::Country) }</div>
+                    <div class="rank-table-header autohide">{ self.view_header("Confirmed", Sort::ConfirmedReversed) }</div>
+                    <div class="rank-table-header autohide">{ self.view_header("Deaths", Sort::DeathsReversed) }</div>
+                    <div class="rank-table-header autohide">{ self.view_header("Recovered", Sort::RecoveredReversed) }</div>
+                    <div class="rank-table-header">{ self.view_header("+/-", Sort::ChangedReversed) }</div>
                     <div class="rank-table-header">{ "Positions" }</div>
 
                     {
@@ -201,6 +197,27 @@ impl App {
         let request = Request::get(API_ENDPOINT).body(Nothing).unwrap();
         let task = self.fetch_service.fetch(request, callback).unwrap();
         self.ft = Some(task);
+    }
+
+    fn view_header(&self, title: &str, sort: Sort) -> Html {
+        let mut arrow_class = "arrows";
+        let sort_target;
+        if sort == self.state.sort {
+            sort_target = sort.reverse().clone();
+            arrow_class = "arrows down";
+        } else {
+            sort_target = sort.clone();
+            if sort_target == self.state.sort.reverse() {
+                arrow_class = "arrows up";
+            }
+        }
+
+        html! {
+            <a href=&sort_target
+            onclick=self.link.callback(move |_| Msg::SetSort(sort_target))>
+              { title } <span class=arrow_class></span>
+            </a>
+        }
     }
 
     fn rank_country(&mut self, country: &str, data: &RawCountryData) -> (isize, isize) {
@@ -256,36 +273,55 @@ impl<'a> Into<Href> for &'a Sort {
     fn into(self) -> Href {
         match *self {
             Sort::Rank => "#/".into(),
-            Sort::RankRevered => "#/rr".into(),
+            Sort::RankReversed => "#/rr".into(),
+            Sort::Country => "#/cy".into(),
+            Sort::CountryReversed => "#/cyr".into(),
             Sort::Confirmed => "#/c".into(),
-            Sort::ConfirmedRevered => "#/cr".into(),
+            Sort::ConfirmedReversed => "#/cr".into(),
             Sort::Deaths => "#/d".into(),
-            Sort::DeathsRevered => "#/dr".into(),
+            Sort::DeathsReversed => "#/dr".into(),
             Sort::Recovered => "#/e".into(),
-            Sort::RecoveredRevered => "#/er".into(),
+            Sort::RecoveredReversed => "#/er".into(),
             Sort::Changed => "#/c".into(),
-            Sort::ChangedRevered => "#/cr".into(),
+            Sort::ChangedReversed => "#/cr".into(),
         }
     }
 }
 
 impl Sort {
-
+    fn reverse(self) -> Sort {
+        match  self {
+            Sort::Rank => Sort::RankReversed,
+            Sort::RankReversed => Sort::Rank,
+            Sort::Country => Sort::CountryReversed,
+            Sort::CountryReversed => Sort::Country,
+            Sort::Confirmed => Sort::ConfirmedReversed,
+            Sort::ConfirmedReversed => Sort::Confirmed,
+            Sort::Deaths => Sort::DeathsReversed,
+            Sort::DeathsReversed => Sort::Deaths,
+            Sort::Recovered => Sort::RecoveredReversed,
+            Sort::RecoveredReversed => Sort::Recovered,
+            Sort::Changed => Sort::ChangedReversed,
+            Sort::ChangedReversed => Sort::Changed,
+        }
+    }
 }
 
 impl State {
     fn sort(&mut self) {
         match self.sort {
             Sort::Rank => self.countries.sort_by(|a, b| a.rank.partial_cmp(&b.rank).unwrap()),
-            Sort::RankRevered => self.countries.sort_by(|a, b| b.rank.partial_cmp(&a.rank).unwrap()),
+            Sort::RankReversed => self.countries.sort_by(|a, b| b.rank.partial_cmp(&a.rank).unwrap()),
+            Sort::Country => self.countries.sort_by(|a, b| a.name.partial_cmp(&b.name).unwrap()),
+            Sort::CountryReversed => self.countries.sort_by(|a, b| b.name.partial_cmp(&a.name).unwrap()),
             Sort::Confirmed => self.countries.sort_by(|a, b| a.confirmed.partial_cmp(&b.confirmed).unwrap()),
-            Sort::ConfirmedRevered => self.countries.sort_by(|a, b| b.confirmed.partial_cmp(&a.confirmed).unwrap()),
+            Sort::ConfirmedReversed => self.countries.sort_by(|a, b| b.confirmed.partial_cmp(&a.confirmed).unwrap()),
             Sort::Deaths => self.countries.sort_by(|a, b| a.deaths.partial_cmp(&b.deaths).unwrap()),
-            Sort::DeathsRevered => self.countries.sort_by(|a, b| b.deaths.partial_cmp(&a.deaths).unwrap()),
+            Sort::DeathsReversed => self.countries.sort_by(|a, b| b.deaths.partial_cmp(&a.deaths).unwrap()),
             Sort::Recovered => self.countries.sort_by(|a, b| a.recovered.partial_cmp(&b.recovered).unwrap()),
-            Sort::RecoveredRevered => self.countries.sort_by(|a, b| b.recovered.partial_cmp(&a.recovered).unwrap()),
+            Sort::RecoveredReversed => self.countries.sort_by(|a, b| b.recovered.partial_cmp(&a.recovered).unwrap()),
             Sort::Changed => self.countries.sort_by(|a, b| a.rank_changed.partial_cmp(&b.rank_changed).unwrap()),
-            Sort::ChangedRevered => self.countries.sort_by(|a, b| b.rank_changed.partial_cmp(&a.rank_changed).unwrap()),
+            Sort::ChangedReversed => self.countries.sort_by(|a, b| b.rank_changed.partial_cmp(&a.rank_changed).unwrap()),
         }
     }
 }
